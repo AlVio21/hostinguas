008b0e7bdfe29e7bd1b4da0d215d5fc4c8ba2c21
AlVio21 <160828042+AlVio21@users.noreply.github.com>
2024-06-29 10:30:14 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -10,22 +10,26 @@ use App\Http\Controllers\API\AuthController;
 
 Route::get('product', [ProductController::class, 'index']);
 Route::post('product', [ProductController::class, 'store']);
-Route::patch('product/{id}', [ProductController::class, 'update']);
+Route::get('product/{id}', [ProductController::class, 'show']);
+Route::put('product/{id}', [ProductController::class, 'update']);
 Route::delete('product/{id}', [ProductController::class, 'destroy']);
 
 Route::get('customer', [CustomerController::class, 'index']);
 Route::post('customer', [CustomerController::class, 'store']);
-Route::patch('customer/{id}', [CustomerController::class, 'update']);
+Route::get('customer/{id}', [CustomerController::class, 'show']);
+Route::put('customer/{id}', [CustomerController::class, 'update']);
 Route::delete('customer/{id}', [CustomerController::class, 'destroy']);
 
 Route::get('order', [OrderController::class, 'index']);
 Route::post('order', [OrderController::class, 'store']);
-Route::patch('order/{id}', [OrderController::class, 'update']);
+Route::get('order/{id}', [OrderController::class, 'show']);
+Route::put('order/{id}', [OrderController::class, 'update']);
 Route::delete('order/{id}', [OrderController::class, 'destroy']);
 
 Route::get('price', [PriceController::class, 'index']);
 Route::post('price', [PriceController::class, 'store']);
-Route::patch('price/{id}', [PriceController::class, 'update']);
+Route::get('price/{id}', [PriceController::class, 'show']);
+Route::put('price/{id}', [PriceController::class, 'update']);
 Route::delete('price/{id}', [PriceController::class, 'destroy']);
 
 Route::post('register', [AuthController::class, 'register']);
